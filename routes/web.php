18cda7c7257d5809622fdf42a9efdb88a0ee35a5
z.barzanooni@yahoo.com <MY_NAME@example.com>
2022-06-09 10:19:40 +0430

    add admin panel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'adminController@index');
+Route::get('/manage', 'adminController@dashbord')->name('manage');
